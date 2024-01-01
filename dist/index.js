12b8929c9a6db2e4cd6bc9373ff0e156e657cb24
baoguoqiang <baoguoqiang@aibots.com.cn>
2024-01-01 17:17:11 +0800

    Update character count display in Footer component

--- dist/index.js
+++ dist/index.js
@@ -17096,7 +17096,7 @@ class C3 extends HTMLElement {
   updateCharacters() {
     this.innerHTML = `
         <div style="display: flex"> 
-            <span> Powered by AiEditor, Characters: ${this.count} </span>
+            <span> 字数统计: ${this.count} </span>
             <div style="width: 20px;height: 20px;overflow: hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 16L6 10H18L12 16Z"></path></svg>
             </div>
